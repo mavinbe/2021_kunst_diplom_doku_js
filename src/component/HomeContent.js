46b931bd4727ab8ae3b1d2f2e223ae79b35e9ac2
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2021-10-08 14:47:26 +0200

    added descritive works.js file and improved one column style

--- src/component/HomeContent.js
+++ src/component/HomeContent.js
@@ -1,31 +1,27 @@
 import React from "react";
 import './HomeContent.css';
 import { Container, Row, Col } from 'react-grid-system';
+import {works_config} from '../description/works';
+
+
 
 export class HomeContent extends React.Component {
-    render() {
-        function getCol(gif_path) {
-            return (
-                <Col md={12} sm={12} xs={12}>
-                    <a href="#" className="tiles" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/media/gifs/${gif_path}")`}}>&nbsp;</a>
+    rendered_tiles = works_config.map((body, key) =>
+        <Col md={12} sm={12} xs={12} >
+            <div className="tile">
+                {body.name}
+                <a href={`/works/${body.slag}`} className="tile-link" style={{backgroundImage: `url("/media/gifs/${body.thumbnail}")`}}>&nbsp;</a>
+            </div>
+        </Col>
+    );
 
-                </Col>
-            );
-        }
+    render() {
+
 
         return (
             <Container>
                 <Row>
-                    {getCol('2016_03_30_IMG_2353_print_v2.gif')}
-                    {getCol('1 2 Screen A.gif')}
-                    {getCol('GOPR3912.gif')}
-                    {getCol('IMG_3131.gif')}
-                    {getCol('Follow_G_small.gif')}
-                    {getCol('Starren Dummy.gif')}
-                    {getCol('look what i\'ve found.gif')}
-                    {getCol('abc2_cuted_no_works.gif')}
-                    {getCol('2020-01-31T17-40-25-000_live.gif')}
-                    {getCol('video.gif')}
+                    {this.rendered_tiles}
                 </Row>
             </Container>
         );
---
+++ src/description/works.js
@@ -0,0 +1,77 @@
+export const works_config = [
+  {
+    "slag": "ot_noise",
+    "name": "o.T.(noise)",
+    "thumbnail": "2016_03_30_IMG_2353_print_v2.gif",
+    "type": "img",
+    //"api_video_code": "2016_03_30_IMG_2353_print_v2.jpg"
+  },
+  {
+    "slag": "radmaschine_version_spielen",
+    "name": "radmaschine version spielen",
+    "thumbnail": "1 2 Screen A.gif",
+    "type": "video",
+  },
+  {
+    "slag": "radmaschine_version_gips",
+    "name": "radmaschine version gips",
+    "thumbnail": "GOPR3912.gif",
+    "type": "video",
+  },
+  {
+    "slag": "radmaschine_version_dark_horses",
+    "name": "radmaschine version \"dark horses\"",
+    "thumbnail": "IMG_3131.gif",
+    "type": "video",
+  },
+  {
+    "slag": "post_hopo_1",
+    "name": "post-hopo 1",
+    "thumbnail": "Starren Dummy.gif",
+    "type": "video",
+  },
+  {
+    "slag": "gleich_gehts_weiter_post_hopo_2)",
+    "name": "gleich gehts weiter - post-hopo 2",
+    "thumbnail": "Follow_G_small.gif",
+    "type": "video",
+  },
+  {
+    "slag": "look_what_i_ve_found",
+    "name": "look what i've found",
+    "thumbnail": "look what i\\'ve found.gif",
+    "type": "video",
+  },
+  {
+    "slag": "no_show",
+    "name": "no show",
+    "thumbnail": "abc2_cuted_no_works.gif",
+    "type": "video",
+  },
+  {
+    "slag": "trek",
+    "name": "trek",
+    "thumbnail": "2020-01-31T17-40-25-000_live.gif",
+    "type": "video",
+  },
+  {
+    "slag": "four_rooms",
+    "name": "four rooms",
+    "thumbnail": "video.gif",
+    "type": "dummy",
+  }
+]
+
+
+/*
+ {getCol('2016_03_30_IMG_2353_print_v2.gif')}
+        {getCol('1 2 Screen A.gif')}
+        {getCol('GOPR3912.gif')}
+        {getCol('IMG_3131.gif')}
+        {getCol('Starren Dummy.gif')}
+        {getCol('Follow_G_small.gif')}
+        {getCol('look what i\'ve found.gif')}
+        {getCol('abc2_cuted_no_works.gif')}
+        {getCol('2020-01-31T17-40-25-000_live.gif')}
+        {getCol('video.gif')}
+ */
