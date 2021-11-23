2344147e5c3e1dd38039eae0e4e0a754059425f2
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2021-11-23 11:06:02 +0100

    commented Navi out

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
             </Route>
           </Switch>
         </div>
-
+        {/*
         <div className="navi">
           <nav>
             <ul>
@@ -53,6 +53,7 @@ export default function App() {
             </ul>
           </nav>
         </div>
+        */}
       </Router>
   );
 }
