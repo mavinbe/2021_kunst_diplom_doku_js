9bbf3083b554b16f1d9d052ea2a9a041517b1729
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-04-04 11:45:11 +0200

    style: black bg, white content, black year boxes

--- src/component/HomeContent.js
+++ src/component/HomeContent.js
@@ -35,7 +35,9 @@ export class HomeContent extends React.Component {
             return [(
                 <Col key={key}  md={12} sm={12} xs={12} >
                     <div className="year">
-                        {key}
+                        <span className="year_span">
+                            {key}
+                        </span>
                     </div>
                 </Col>
             ),rendered_tiles] ;
@@ -50,16 +52,18 @@ export class HomeContent extends React.Component {
 
 
         return (
-            <Container>
-                <Row>
-                    <Col key="qr"  md={12} sm={12} xs={12} >
-                        <div className="qr_wrapper">
-                            <img className="qr"  src={`media/img/qr.png`} alt="Logo" />
-                        </div>
-                    </Col>
-                    {this.year_sections}
-                </Row>
-            </Container>
+            <div className="content_wrapper">
+                <Container>
+                    <Row>
+                        <Col key="qr"  md={12} sm={12} xs={12} >
+                            <div className="qr_wrapper">
+                                <img className="qr"  src={`media/img/qr.png`} alt="Logo" />
+                            </div>
+                        </Col>
+                        {this.year_sections}
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
